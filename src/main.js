c705c9dbb01ae6ea2d823c50004449ed313c3a61
Andrew Stepanov <stepanov.management@gmail.com>
2023-08-18 15:45:33 +0500

    Поправлены отрисовки предыдущего задания

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import TripInfoView from './view/trip-info-view.js';
-import TripCostView from './view/trip-cost-view.js';
 import FilterView from './view/filter-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import {render} from './render.js';
 
-const siteTripInfoElement = document.querySelector('.trip-info');
-const siteTripControlsElement = document.querySelector('.trip-controls');
+const siteTripInfoElement = document.querySelector('.trip-main');
+const siteTripControlsElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.trip-events');
 const eventsPresenter = new EventsPresenter({eventsContainer: siteMainElement});
 
-render(new TripInfoView(), siteTripInfoElement);
-render(new TripCostView(), siteTripInfoElement);
+render(new TripInfoView(), siteTripInfoElement, 'AFTERBEGIN');
 render(new FilterView(), siteTripControlsElement);
 
 eventsPresenter.init();
