232e54cfb5066735a232d68b33001fb28b46f811
Andrew Stepanov <stepanov.management@gmail.com>
2023-10-02 20:57:24 +0500

    Исправлен баг с favorites

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -121,7 +121,7 @@ export default class PointPresenter {
 
   #handleFavoriteClick = () => {
     this.#handleDataChange(
-      UserAction.UPDATE_TASK,
+      UserAction.UPDATE_POINT,
       UpdateType.MINOR,
       {...this.#point, isFavorite: !this.#point.isFavorite},
     );
