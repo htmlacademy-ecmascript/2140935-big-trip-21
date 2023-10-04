ad4baf0fafe479e1168076dba522e828d52bfaab
Andrew Stepanov <stepanov.management@gmail.com>
2023-10-04 19:59:54 +0500

    Убраны consolelog лишние

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -70,7 +70,6 @@ export default class MainPresenter {
   get points() {
     const filterType = this.#filterModel.filter;
     const points = this.#pointsModel.points;
-    console.log(points);
     const filteredPoints = filter[filterType](points);
     switch (this.#currentSortType) {
       case SortType.DAY:
