4f66022f60430d5aa6eec62b37732e8297073237
Andrew Stepanov <stepanov.management@gmail.com>
2023-10-09 08:14:11 +0500

    Убран лишний he

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -143,7 +143,7 @@ function createEditPointTemplate(data, editMode) {
               <span class="visually-hidden">Price</span>
               &euro;
             </label>
-            <input class="event__input  event__input--price" id="event-price-${id}" type="number" name="event-price" value="${he.encode(basePrice)}" ${isDisabled ? 'disabled' : ''}>
+            <input class="event__input  event__input--price" id="event-price-${id}" type="number" name="event-price" value="${basePrice}" ${isDisabled ? 'disabled' : ''}>
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit" ${isDisabled ? 'disabled' : ''}>${isSaving ? 'Saving' : 'Save'}</button>
